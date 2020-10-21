70d9e5e89483e3afd7033b806882080c10a82e42
Callie Stump <callie@stu.mp>
2020-10-21 13:21:03 -0400

    capitalizes inputted name
    Co-authored-by: Ryland Adams <rylandadams@yahoo.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   $("#formOne").submit(function(event) {
     const person1Input = $("input#person1").val();
 
-    $(".person1").text(person1Input);
+    $(".person1").text (person1Input.toUpperCase());
 
     $("#story").show();
 
